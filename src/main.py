54f92685e91fbd81e8520e16d6209a8f684d29ad
doomwall <tuomo.valkila@gmail.com>
2025-09-11 10:58:32 +0300

    markov order validation

--- src/main.py
+++ src/main.py
@@ -9,17 +9,21 @@ def main():
     feed = pars.parser(source)
 
     print("Welcome to tekstigeneraattori")
-    order = input("Give me Markov order: ")
-    trie = Trie(n=int(order))
-    trie.insert_helper(feed)
-
+    while True:
+        order = input("Give me Markov order: ")
+        try:
+            trie = Trie(n=int(order))
+            trie.insert_helper(feed)
+            break
+        except:
+            print("Please give a number")
+    
     while True:
         word = input("Give me a word: ")
         if word == "":
             break
 
         amount = input("Give me how many words you want: ")
-        
 
         prediction = trie.predict([word], int(amount))
         collected = pars.collect_from_list(prediction)
